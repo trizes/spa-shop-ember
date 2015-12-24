185ed1aa8c2e662c3ff5b23a6202bd8c08a9249e
Dmitriy Shirshov <dmitriy.shirshov@ecommerce.com>
2015-12-25 01:37:02 +0200

    Removed Bootstrap

--- ekoterm-org-ember/ember-cli-build.js
+++ ekoterm-org-ember/ember-cli-build.js
@@ -19,7 +19,20 @@ module.exports = function(defaults) {
   // modules that you would like to import into your application
   // please specify an object with the list of modules as keys
   // along with the exports of each module as its value.
-  app.import('bower_components/bootstrap/dist/css/bootstrap.css');
+  // app.import({
+  //   development: 'bower_components/bootstrap/dist/css/bootstrap.css',
+  //   production:  'bower_components/bootstrap/dist/css/bootstrap.min.css'
+  // });
+  // app.import({
+  //   development: 'bower_components/bootstrap/dist/css/bootstrap-theme.css',
+  //   production:  'bower_components/bootstrap/dist/css/bootstrap-theme.min.css'
+  // });
+  // // app.import({
+  // //   development: 'bower_components/bootstrap/dist/js/bootstrap.js',
+  // //   production:  'bower_components/bootstrap/dist/js/bootstrap.min.js'
+  // // });
+  
+  // app.import('bower_components/bootstrap/dist/js/bootstrap.min.js');
 
   return app.toTree();
 };
